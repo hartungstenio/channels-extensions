f5ea1b3849d41e30ad20d7595dbabe92698633a5
Christian Hartung <6785871+hartungstenio@users.noreply.github.com>
2026-01-02 18:52:34 -0300

    style: improve type-hints of site middleware

--- src/channels_extensions/sites.py
+++ src/channels_extensions/sites.py
@@ -1,11 +1,14 @@
 import asyncio
 from copy import copy
-from typing import Any
+from typing import TYPE_CHECKING
 
-from asgiref.typing import ASGIApplication, ASGIReceiveCallable, ASGISendCallable
+from asgiref.typing import ASGIApplication, ASGIReceiveCallable, ASGISendCallable, Scope
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
 from django.contrib.sites.models import Site
+
+if TYPE_CHECKING:
+    from channels.utils import _ChannelApplication
 
 
 class CurrentSiteMiddleware(BaseMiddleware):
@@ -17,10 +20,15 @@ class CurrentSiteMiddleware(BaseMiddleware):
         super().__init__(inner)
         self._lock = asyncio.Lock()
 
-    async def __call__(self, scope: dict[str, Any], receive: ASGIReceiveCallable, send: ASGISendCallable) -> Any:  # type: ignore[override]
+    async def __call__(
+        self,
+        scope: Scope,
+        receive: ASGIReceiveCallable,
+        send: ASGISendCallable,
+    ) -> "_ChannelApplication":
         if "site" not in scope:
             async with self._lock:
                 if "site" not in scope:
                     scope = copy(scope)
-                    scope["site"] = await database_sync_to_async(Site.objects.get_current)()
+                    scope["site"] = await database_sync_to_async(Site.objects.get_current)()  # type: ignore[typeddict-unknown-key]
         return await super().__call__(scope, receive, send)  # type: ignore[arg-type]
